2522ae2c1188b19b29a733853eb3e2b1ae5c2d3a
mikael <mikael.programing.c@gmail.com>
2024-06-09 09:30:55 +0100

    f

---
+++ ChallenegeApp/EmployeeBase.cs
@@ -0,0 +1,27 @@
+﻿namespace ChallenegeApp
+{
+    public abstract class EmployeeBase : IEmployee
+    {
+        public EmployeeBase(string name, string surname)
+        {
+            this.Name = name;
+            this.Surname = surname;
+        }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public abstract void AddGrade(float grade);
+
+        public abstract void AddGrade(double grade);
+
+        public abstract void AddGrade(int grade);
+
+        public abstract void AddGrade(char grade);
+
+        public abstract void AddGrade(string grade);
+
+        public abstract Statistics GetStatistics();
+    }
+}
